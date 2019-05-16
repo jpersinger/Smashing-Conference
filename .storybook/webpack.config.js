916512d3e5c81fe17ce05555f199d06d859cc5bd
Julie Persinger <julie.persinger@willowtreeapps.com>
2019-05-16 10:35:45 -0400

    FINALLY got storybook to play nice with Gatsby

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,135 +1,57 @@
-const path = require("path");
-const SRC_PATH = path.join(__dirname, '../src');
+require("imports-loader")
 
-module.exports = ({config}) => {
+const path = require("path")
+const SRC_PATH = path.join(__dirname, "../src")
+
+module.exports = ({ config }) => {
   config.module.rules.push({
     test: /\.(ts|tsx)$/,
     include: [SRC_PATH],
-      use: [
-        {
-          loader: require.resolve('awesome-typescript-loader'),
-          options: {
-            configFileName: './.storybook/tsconfig.json'
-          }
-        },
-        { loader: require.resolve('react-docgen-typescript-loader') }
-      ]
-  });
-
-  const cssLoaderIndex = config.module.rules.findIndex(
-    rule => rule.test.source === `\\.css$`
-  );
-  config.module.rules[cssLoaderIndex].exclude = /\.module\.css$/;
-
-  // config.module.rules.push({
-  //   test: /\.module\.css$/,
-  //   loaders: [
-  //     require.resolve('style-loader'),
-  //     {
-  //       loader: require.resolve('css-loader'),
-  //       options: {
-  //         importLoaders: 1,
-  //         modules: true,
-  //         localIdentName: '[name]__[local]___[hash:base64:5]',
-  //       },
-  //     },
-  //   ],
-  // });
-  config.module.rules.push({
-    test: /\.css$/,
     use: [
       {
-        loader: 'style-loader',
+        loader: require.resolve("awesome-typescript-loader"),
+        options: {
+          configFileName: "./.storybook/tsconfig.json",
+        },
       },
+      { loader: require.resolve("react-docgen-typescript-loader") },
+    ],
+  })
+
+  // Find Storybook's default CSS processing rule
+  const cssLoaderIndex = config.module.rules.findIndex(
+    rule => rule.test.source === `\\.css$`
+  )
+
+  if (!Number.isInteger(cssLoaderIndex))
+    throw new Error("Could not find Storybook's CSS loader")
+
+  // Exclude CSS Modules from Storybook's standard CSS processing
+  config.module.rules[cssLoaderIndex].exclude = /\.module\.css$/
+
+  // Add specific loader rule for CSS Modules
+  config.module.rules.push({
+    test: /\.module\.css$/,
+    use: [
+      { loader: `style-loader` },
+      { loader: `css-modules-typescript-loader` },
       {
-        loader: 'css-loader',
+        loader: "css-loader",
         options: {
           modules: true,
         },
       },
     ],
-  });
-  // config.module.rules.push({
-  //       test: /\.module\.css$/,
-  //       use: [
-  //         { loader: `style-loader` },
-  //         {
-  //           loader: "css-loader",
-  //           options: {
-  //             modules: true,
-  //             importLoaders: 1,
-  //             localIdentName: "[path]-[local]-[hash:base64:5]"
-  //           }
-  //         },
-  //         // {
-  //         //   // Use newer versions of postcss tools
-  //         //   loader: `postcss-loader-2`,
-  //         //   options: {
-  //         //     plugins: loader => [
-  //         //       require("postcss-import-11")({ root: loader.resourcePath }),
-  //         //       require("postcss-cssnext-3")()
-  //         //     ]
-  //         //   }
-  //         // }
-  //       ],
-  //       include: path.resolve(__dirname, "../src")
-  //     });
+    include: path.resolve(__dirname, "../src"),
+  })
 
-  config.resolve.extensions.push('.ts', '.tsx', '.css');
-  return config;
-};
+  // ignore gatsby-link's global `__loader` variable
+  config.module.rules.push({
+    test: require.resolve("gatsby-link"),
+    loaders: ["imports-loader?___loader=>{enqueue:function(){}}"],
+  })
 
-// require("imports-loader");
+  config.resolve.extensions.push(".ts", ".tsx", ".css")
 
-// const path = require("path");
-// const genDefaultConfig = require("@storybook/react/dist/server/config/defaults/webpack.config.js");
-
-// module.exports = (baseConfig, env) => {
-//   const config = genDefaultConfig(baseConfig, env);
-
-//   // Find Storybook's default CSS processing rule
-//   const cssLoaderIndex = config.module.rules.findIndex(
-//     rule => rule.test.source === `\\.css$`
-//   );
-
-//   if (!Number.isInteger(cssLoaderIndex))
-//     throw new Error("Could not find Storybook's CSS loader");
-
-//   // Exclude CSS Modules from Storybook's standard CSS processing
-//   config.module.rules[cssLoaderIndex].exclude = /\.module\.css$/;
-
-//   // Add specific loader rule for CSS Modules
-//   config.module.rules.push({
-//     test: /\.module\.css$/,
-//     use: [
-//       { loader: `style-loader` },
-//       {
-//         loader: "css-loader",
-//         options: {
-//           modules: true,
-//           importLoaders: 1,
-//           localIdentName: "[path]-[local]-[hash:base64:5]"
-//         }
-//       },
-//       {
-//         // Use newer versions of postcss tools
-//         loader: `postcss-loader-2`,
-//         options: {
-//           plugins: loader => [
-//             require("postcss-import-11")({ root: loader.resourcePath }),
-//             require("postcss-cssnext-3")()
-//           ]
-//         }
-//       }
-//     ],
-//     include: path.resolve(__dirname, "../src")
-//   });
-
-//   // ignore gatsby-link's global `__loader` variable
-//   config.module.rules.push({
-//     test: require.resolve("gatsby-link"),
-//     loaders: ["imports-loader?___loader=>{enqueue:function(){}}"]
-//   });
-
-//   return config;
-// };
+  return config
+}
